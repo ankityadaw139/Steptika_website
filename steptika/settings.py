cd3c539e7f36f0d285622725f1f3b98836f7d626
mohnish <mohnishvats@gmail.com>
2021-03-14 19:58:30 +0530

    2nd commit

--- steptika/settings.py
+++ steptika/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_fe5f@tnw)^ip*5ul8^ag$@xf4=^l2xjg-du0e1kj9_7ngl1o7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
